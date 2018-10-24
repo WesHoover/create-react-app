833e0b0b5204306fa540ac70cdec03fd7ba44a81
Shelby Wilson <shelby.wilson@dimensional.com>
2018-10-24 16:04:34 -0500

    add example storybook story

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 // import * as moment from 'moment';
 // import * as d3 from 'd3';
 import './app.css';
-import GridDemo from './components/vendor/grid/gridDemo';
+import GridDemo from 'vendor/grid/gridDemo';
+import ImageLogo from 'utility/images/ImageLogo';
 
 class App extends Component {
 	render() {
 	return (
 		<div className="app">
+			<ImageLogo />
 			<GridDemo />
 		</div>
 	);
